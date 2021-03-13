c1008bf42bc4fe89620c310b3f0ff5ef945440e7
Lucie20 <60733340+Lucie20@users.noreply.github.com>
2021-03-13 11:04:01 +0100

    ukol 1 hotovo

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // tady je místo pro náš program
 
-document.querySelector("#vysledek").innerHTML = secti(4, 5);
+// document.querySelector("#vysledek").innerHTML = secti(4, 5);
+
 
 /**
  * Sečte dvě čísla na vstupu a vrátí výsledek
@@ -16,6 +17,25 @@ function secti(a, b) {
 /**
  * Upozorní uživatele při spuštění.
  */
-function upozorni() {
-  alert("Gratulace, právě jsi spustila tuto funkci!");
+
+// function upozorni() {
+//   alert("Gratulace, právě jsi spustila tuto funkci!");
+// }
+
+/* Vytvořte tlačítko, při jehož stisknutí se změní barva <strong>Čtvereček</strong> na zelenou.*/
+
+document.querySelector('.ctverecek').style.backgroundColor = 'green';
+
+/* Upravte kód tak, aby se výsledek funkce <em>secti()</em> zobrazil v prvku s ID <strong>vysledek</strong> až po stiknutí nově přidaného tlačítka "Zobraz výsledek". */
+
+function zobraz() {
+  document.querySelector("#vysledek").innerHTML = secti(4, 5);
 }
+
+/* Uprav funkci <em>upozorni()</em> tak, aby se navíc vypisoval text i do konzole a také nahradil text v elementu <strong>Čtvereček</strong>. */
+
+function upozorni() {
+  console.log('Gratulace, právě jsi spustila tuto funkci!');
+  document.querySelector('.ctverecek').textContent = 'Gratulace, právě jsi spustila tuto funkci!';
+}
+
